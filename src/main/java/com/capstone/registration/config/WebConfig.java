54aaded0b075d554c045c10c9930394239e4df49
jsp0303 <jsp6616@gmail.com>
2026-04-29 15:32:01 +0900

    백/프론트 연동

---
+++ src/main/java/com/capstone/registration/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.capstone.registration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 3000번(수강신청), 3001번(장바구니) 접속 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
